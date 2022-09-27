0a3638232f7c60efd089def8068fea4b349a9f0c
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-09-27 15:55:31 -0600

    Update loopHoriz.sh

--- loopHoriz.sh
+++ loopHoriz.sh
@@ -1,18 +1,15 @@
 # run on casper
 set -e
 
-iyear=1979
-imonth=01
-
-#for iyear in {1979..1980}; do
-#       for imonth in {01..12}; do
+for iyear in {1979..1984}; do
+        for imonth in {01..12}; do
                 for ifile0 in /glade/scratch/sglanvil/ERA5regridded/ERA5_$iyear-$imonth-*.nc; do
                         idate0=`echo $ifile0 | rev | cut -c 4-19 | rev`
                         export ifile0
                         export idate0
                         qsub -v ifile0,idate0 /glade/work/sglanvil/CCR/regrid/ERA5horiz.sh
                 done
-#       done
-#done
+        done
+done
 
 
