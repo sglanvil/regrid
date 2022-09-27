ed8b6944851c1c01fc2a2bf2d6c234403a8675c3
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-09-27 15:56:15 -0600

    Update ERA5horiz.sh

--- ERA5horiz.sh
+++ ERA5horiz.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -N ERA5horiz 
+#PBS -N regrid_horiz
 #PBS -A CESM0021 
-#PBS -l select=1:ncpus=1:mem=200GB
-#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=1:mem=100GB
+#PBS -l walltime=00:10:00
 #PBS -k eod
 #PBS -j oe
 #PBS -q casper
@@ -11,7 +11,7 @@
 module load nco
 module load ncl
 
-echo ${ifile0}
-ncl -Q ifile=${ifile0} /glade/work/sglanvil/CCR/regrid/ERA5horiz.ncl
+ncl -Q 'ifile="'$ifile0'"' 'idate="'$idate0'"' /glade/work/sglanvil/CCR/regrid/ERA5horiz.ncl
 
 exit
+
