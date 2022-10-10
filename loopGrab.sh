8f1bdee0da7da9f51dc7a05dcbfbe8effec9e407
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-10-10 14:52:33 -0600

    Update loopGrab.sh

--- loopGrab.sh
+++ loopGrab.sh
@@ -1,11 +1,10 @@
 # run on casper
 set -e
 
-for iyear in {1979..1989}; do
+for iyear in {2000..2020}; do
         for imonth in {01..12}; do
                 export iyear
                 export imonth
                 qsub -v iyear,imonth /glade/work/sglanvil/CCR/regrid/ERA5grab.sh
         done
 done
-
