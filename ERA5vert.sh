b1f57fb72c2726cde764f5b9585fc7d1241981fa
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-10-10 14:55:04 -0600

    Create ERA5vert.sh

---
+++ ERA5vert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -N regrid_vert
+#PBS -A CESM0021 
+#PBS -l select=1:ncpus=1:mem=100GB
+#PBS -l walltime=12:00:00
+#PBS -k eod
+#PBS -j oe
+#PBS -q casper
+#PBS -m abe
+
+iyear=1984
+
+module load nco
+module load ncl
+
+for ifile0 in /glade/scratch/sglanvil/ERA5regridded/ERA5horiz_$iyear*; do
+        idate0=`echo $ifile0 | rev | cut -c 4-19 | rev`
+        echo $idate0
+        export ifile0
+        export idate0
+        ncl -Q 'ifile="'$ifile0'"' 'idate="'$idate0'"' /glade/work/sglanvil/CCR/regrid/ERA5vert.ncl
+done
+
+exit
