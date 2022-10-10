6220bdc7a8e9039ebf38c6adeda197d90c3151a3
Sasha Glanville <51757958+sglanvil@users.noreply.github.com>
2022-10-10 14:54:11 -0600

    Update ERA5horiz.sh

--- ERA5horiz.sh
+++ ERA5horiz.sh
@@ -2,16 +2,23 @@
 #PBS -N regrid_horiz
 #PBS -A CESM0021 
 #PBS -l select=1:ncpus=1:mem=100GB
-#PBS -l walltime=00:10:00
+#PBS -l walltime=12:00:00
 #PBS -k eod
 #PBS -j oe
 #PBS -q casper
 #PBS -m abe
 
+iyear=2020
+
 module load nco
 module load ncl
 
-ncl -Q 'ifile="'$ifile0'"' 'idate="'$idate0'"' /glade/work/sglanvil/CCR/regrid/ERA5horiz.ncl
+for ifile0 in /glade/scratch/sglanvil/ERA5regridded/ERA5_$iyear*; do
+        idate0=`echo $ifile0 | rev | cut -c 4-19 | rev`
+        echo $idate0
+        export ifile0
+        export idate0
+        ncl -Q 'ifile="'$ifile0'"' 'idate="'$idate0'"' /glade/work/sglanvil/CCR/regrid/ERA5horiz.ncl
+done
 
 exit
-
